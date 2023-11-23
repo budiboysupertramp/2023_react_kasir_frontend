f78874441b23c716e6b81e6bc60b6433bae6aaf0
Setia Budi <budiboy195@gmail.com>
2023-11-23 15:14:21 +0700

    test api

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,3 +1,3 @@
 // export const API_URL = "https://kasir-frontend.vercel.app/";
 // export const API_URL = "https://kasir-backend.vercel.app/";
-export const API_URL = "http://mybackend.cloud/db.json/";
+export const API_URL = "https://mybackend.cloud/db.json/";
